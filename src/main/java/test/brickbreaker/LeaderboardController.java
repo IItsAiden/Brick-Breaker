1ac361b9b5215a416a88b7b3264e176bc61ff0a9
user <vunyc9623@gmail.com>
2021-12-06 00:06:16 +0800

    Fix the leaderboard button not working

---
+++ src/main/java/test/brickbreaker/LeaderboardController.java
@@ -0,0 +1,63 @@
+package test.brickbreaker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class LeaderboardController implements Initializable {
+
+
+    ArrayList<String> list = new ArrayList<String>();
+
+
+    @FXML
+    private ListView<String> listView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Leaderboard();
+        list.sort(Collections.reverseOrder());
+        System.out.println(list);
+        listView.getItems().addAll(list);
+    }
+
+    public void back(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HomeMenu.class.getResource("HomeMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void Leaderboard() {
+        try {
+            File myObj = new File("Leaderboard.txt");
+            int counter = 0;
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNext() && counter < 10) {
+                list.add(myReader.next());
+                counter++;
+            }
+            myReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
