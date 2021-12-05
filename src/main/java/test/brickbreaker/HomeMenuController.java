5e6bcc9a5710785879f223aaea3d15d01d6cdd46
user <vunyc9623@gmail.com>
2021-12-05 23:40:02 +0800

    Fix the exit button not working

--- src/main/java/test/brickbreaker/HomeMenuController.java
+++ src/main/java/test/brickbreaker/HomeMenuController.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,5 +20,17 @@ public class HomeMenuController {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+    }
+
+    public void exit(ActionEvent event) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("Are you sure?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.close();
+        }
     }
 }
